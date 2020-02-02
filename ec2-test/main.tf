3e55d67a48299462ef66524d8671f4d395e9ff11
Oleksandr Kulbida <oleksandr.kulbida@intellecteu.com>
2020-02-02 14:35:19 +0200

    ec2 test example

--- ec2-test/main.tf
+++ ec2-test/main.tf
@@ -1,4 +1,31 @@
 resource "aws_instance" "myinstance" {
-  ami                     = "ami-062f7200baf2fa504"
-  instance_type           = "t2.micro"
+  ami           = "ami-062f7200baf2fa504"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.allow_http.name}"]
+
+  tags = {
+    Name = "SG EC2"
+  }
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
 }
