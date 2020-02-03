bd60b947bf431a697b7300a4b1054d591a11c38a
Oleksandr Kulbida <oleksandr.kulbida@intellecteu.com>
2020-02-03 10:34:09 +0200

    add let's encrypt simple example without wildcard

--- ec2-test/aws-route53.tf
+++ ec2-test/aws-route53.tf
@@ -4,9 +4,19 @@ data "aws_route53_zone" "dev" {
   private_zone = false
 }
 
-resource "aws_route53_record" "front" {
+resource "aws_route53_record" "front1" {
   provider = "aws.us-east-1"
   name     = "k1.${var.domain}"
+  type     = "A"
+  ttl      = 3600
+  records  = ["${aws_instance.myinstance.public_ip}"]
+  zone_id  = "${data.aws_route53_zone.dev.id}"
+}
+
+
+resource "aws_route53_record" "front2" {
+  provider = "aws.us-east-1"
+  name     = "k2.${var.domain}"
   type     = "A"
   ttl      = 3600
   records  = ["${aws_instance.myinstance.public_ip}"]
--- ec2-test/setup.sh
+++ ec2-test/setup.sh
@@ -1,7 +1,43 @@
 #!/bin/bash
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt update
-sudo apt install -y nginx
-sudo apt install software-properties-common
+sudo apt install -y nginx software-properties-common
 sudo apt install -y certbot python3-certbot-nginx
+cat > /etc/nginx/sites-enabled/default << __EOF
+# Master nginx on virtual machine
+server {
+
+  listen 80;
+  listen [::]:80;
+
+  server_name k1.k8slabs.online;
+
+
+  location / {
+                proxy_pass http://localhost:3000;
+                proxy_http_version  1.1;
+                proxy_set_header Connection "upgrade";
+                proxy_redirect http:// https://;
+  }
+}
+
+server {
+
+    listen 80;
+    listen [::]:80;
+
+    server_name k2.k8slabs.online;
+
+    location / {
+                proxy_pass http://localhost:3001;
+                proxy_http_version  1.1;
+                proxy_set_header Connection "upgrade";
+                proxy_redirect http:// https://;
+        }
+}
+
+
+__EOF
 sudo certbot --nginx --noninteractive  --redirect --agree-tos --email webstd.design@gmail.com -d k1.k8slabs.online
+sudo certbot --nginx --noninteractive  --redirect --agree-tos --email webstd.design@gmail.com -d k2.k8slabs.online
+# sudo certbot --nginx --noninteractive  --agree-tos --email webstd.design@gmail.com -d k1.k8slabs.online
